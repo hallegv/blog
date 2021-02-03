a0d8c34c90c1904a481eabc03a10af51f6e71b1a
hallegv <hallevogelpohl@gmail.com>
2021-02-03 15:00:49 -0800

    added route to About page

---
+++ src/About.jsx
@@ -0,0 +1,20 @@
+import { React } from "react";
+
+export default function About() {
+  return (
+    <div className="about">
+      <div class="container">
+        <div class="row align-items-center my-5">
+          <div class="col-lg-5">
+            <h1 class="font-weight-light">About</h1>
+            <p>
+              Hi everyone! I'm Halle Vogelpohl, a junior CS major at Loyola
+              Marymount University. Here I'm creating a simple blog to practice
+              React fundamentals.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import PostForm from "./PostForm";
 import PostList from "./PostList";
 import PostModal from "./PostModal";
+import About from "./About";
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -36,28 +38,46 @@ function App() {
     setShow(false);
   }
   return (
-    <Container className="p-3 mb-2 bg-info text-black">
-      <Row className="justify-content-md-center" style={{ marginTop: 60 }}>
-        <Col className="p-3 mb-2 bg-info text-white" xs lg="2">
-          <h1 style={{ textAlign: "center" }}> Halle's Blog!</h1>
-        </Col>
-      </Row>
-      <Row style={{ marginTop: 20 }}>
-        <Col xs lg="2">
-          <PostForm addPostToList={addPostToList} />
-        </Col>
-      </Row>
-      <Row className="justify-content-md-center" style={{ marginTop: 20 }}>
-        <Col xs lg="2">
-          <PostList
-            posts={posts}
-            deletePost={deletePost}
-            displayPostInModal={displayPostInModal}
-          />
-        </Col>
-      </Row>
-      <PostModal closePostModal={closePostModal} show={show} />
-    </Container>
+    <>
+      <Container className="p-3 mb-2 bg-info text-black">
+        <Row className="justify-content-md-center" style={{ marginTop: 60 }}>
+          <Col className="p-3 mb-2 bg-info text-white" xs lg="2">
+            <h1 style={{ textAlign: "center" }}> Halle's Blog!</h1>
+          </Col>
+        </Row>
+
+        <Router>
+          <div className="container">
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+              </ul>
+            </nav>
+            <Switch>
+              <Route path="/about" exact component={() => <About />} />
+            </Switch>
+          </div>
+        </Router>
+
+        <Row style={{ marginTop: 20 }}>
+          <Col xs lg="2">
+            <PostForm addPostToList={addPostToList} />
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center" style={{ marginTop: 20 }}>
+          <Col xs>
+            <PostList
+              posts={posts}
+              deletePost={deletePost}
+              displayPostInModal={displayPostInModal}
+            />
+          </Col>
+        </Row>
+        <PostModal closePostModal={closePostModal} show={show} />
+      </Container>
+    </>
   );
 }
 
